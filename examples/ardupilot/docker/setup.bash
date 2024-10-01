3cf489502b69d00b71b26463bd2c1b3d64a43716
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2024-10-01 13:44:30 -0500

    Move up environment variable for Daikon

---
+++ examples/ardupilot/docker/setup.bash
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-11-openjdk-amd64/lib/server
+export DAIKONDIR=~/daikon-5.8.10
+source $DAIKONDIR/scripts/daikon.bashrc
+export CLASSPATH=$CLASSPATH:~/daikon-5.8.10/java/daikon:/~/radler/examples/ardupilot/jni:~/beepbeep-3-examples/beepbeep-3-examples.jar:~/daikon-5.8.10/daikon.jar:~/daikon-5.8.10/java/dcomp_rt.jar:~/daikon-5.8.10/java/ChicoryPremain.jar:~/daikon-5.8.10/java/dcomp_premain.jar:~/apache-log4j-2.17.2-bin/log4j-api-2.17.2.jar:~/apache-log4j-2.17.2-bin/log4j-core-2.17.2.jar
+export PATH=$PATH:~/apache-log4j-2.17.2-bin
