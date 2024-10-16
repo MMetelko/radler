a67415917ac6acaa7c130774df4f17f96fa27d50
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2024-10-16 10:53:10 -0500

    Current build of docker - ardupilot tools start, working on graphics setup

--- examples/ardupilot/docker/ardupilot_entrypoint.sh
+++ examples/ardupilot/docker/ardupilot_entrypoint.sh
@@ -2,25 +2,28 @@
 set -e
 
 # setup ardupilot environment
-source /home/${USER}/.ardupilot_env
+source ~/.ardupilot_env
+
+# Finish pulling the ardupilot repository
+git submodule update --init --recursive
 
 # setup ros2 environment
 source "$ROS2_PREFIX/$ROS2_DISTRO/setup.bash"
 
 # build radler house_thermo example
-cd /root/radler 
+cd ~/radler 
 git pull
 ./radler.sh --ws_dir $ROS2_WS/src compile examples/ardupilot/afs.radl --plant plant --ROS
 cd $ROS2_WS
 colcon build \
     --cmake-args \
-      -DSECURITY=ON --no-warn-unused-cli \
+    -DSECURITY=ON --no-warn-unused-cli \
     --symlink-install
 source "$ROS2_WS/install/local_setup.bash"
 
 #sros2
 # From vagrant/sros_keystore.bash
-cd $ROS2_WS
+cd ~
 ros2 security create_keystore sros2_keys
 ros2 security create_key sros2_keys /afs/mavros
 ros2 security create_key sros2_keys /afs/gateway
@@ -29,13 +32,13 @@ ros2 security create_key sros2_keys /afs/afs_esp
 ros2 security create_key sros2_keys /afs/afs_log
 
 # From vagrant sros_env.bash
-mkdir -p $ROS2_WS/install/afs/lib/afs
-cd $ROS2_WS/install/afs/lib/afs
+mkdir -p ~/radler/install/afs/lib/afs
+cd ~/radler/install/afs/lib/afs
 ln -sf ../../../../build/afs/gateway .
 ln -sf ../../../../build/afs/afs_battery .
 ln -sf ../../../../build/afs/afs_esp .
 ln -sf ../../../../build/afs/afs_log .
-cd -
+cd /ardupilot
 
 exec "$@"
 
