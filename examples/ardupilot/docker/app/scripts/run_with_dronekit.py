61ccedf8c45a66bf03c6098a71d8e9e7b7a5e110
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2025-02-10 17:02:54 -0600

    Editing mistakes found ...

--- examples/ardupilot/docker/app/scripts/run_with_dronekit.py
+++ examples/ardupilot/docker/app/scripts/run_with_dronekit.py
@@ -21,27 +21,27 @@ class DroneController:
         Arms the vehicle and flies to aTargetAltitude.
         """
         print("Basic pre-arm checks")
-        while not vehicle.is_armable:
+        while not self.vehicle.is_armable:
             print(" Waiting for vehicle to initialize...")
             time.sleep(1)
 
         print("Arming motors")
-        vehicle.mode = VehicleMode("GUIDED")
-        while vehicle.mode != 'GUIDED':
+        self.vehicle.mode = VehicleMode("GUIDED")
+        while self.vehicle.mode != 'GUIDED':
             print(" Waiting for guiding mode...")
             time.sleep(1)
 
-        vehicle.armed = True
-        while not vehicle.armed:
+        self.vehicle.armed = True
+        while not self.vehicle.armed:
             print(" Waiting for arming...")
             time.sleep(1)
 
         print("Taking off!")
-        vehicle.simple_takeoff(aTargetAltitude)
+        self.vehicle.simple_takeoff(aTargetAltitude)
 
         while True:
-            print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-            if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print(" Altitude: ", self.vehicle.location.global_relative_frame.alt)
+            if self.vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
                 print("Reached target altitude")
                 break
             time.sleep(1)
@@ -67,10 +67,10 @@ class DroneController:
         """
         Move the vehicle to a position `dNorth` and `dEast` meters away from the current position, maintaining altitude change `dAlt`.
         """
-        current_location = vehicle.location.global_relative_frame
+        current_location = self.vehicle.location.global_relative_frame
         target_location = get_location_offset_meters(current_location, dNorth, dEast, dAlt)
         print(f"Moving to position (NORTH: {dNorth}m, EAST: {dEast}m, ALT: {dAlt}m)")
-        self.simple_goto(target_location)
+        self.vehicle.simple_goto(target_location)
         # Adjust time to ensure vehicle reaches the target
         #MM TODO: adjust this value to meet the needs of the radler system.  In other words, make sure the return is due to battery level, not block by this timeout.
         time.sleep(60)
@@ -80,16 +80,16 @@ class DroneController:
         Command the vehicle to land and wait until it reaches an altitude of 0 meters.
         """
         print("Initiating landing...")
-        self.mode = VehicleMode("LAND")
-        while self.mode != 'LAND':
+        self.vehicle.mode = VehicleMode("LAND")
+        while self.vehicle.mode != 'LAND':
             print(" Waiting for landing mode...")
             time.sleep(1)
 
         # Wait until the vehicle reaches an altitude of 0 meters
         while True:
-            print(" Altitude: ", self.location.global_relative_frame.alt)
-            if self.location.global_relative_frame.alt <= 0.1:
-                print("Landed. Altitude: ", self.location.global_relative_frame.alt)
+            print(" Altitude: ", self.vehicle.location.global_relative_frame.alt)
+            if self.vehicle.location.global_relative_frame.alt <= 0.1:
+                print("Landed. Altitude: ", self.vehicle.location.global_relative_frame.alt)
                 break
             time.sleep(1)
 
@@ -97,14 +97,14 @@ class DroneController:
         # Main script starts here
         try:
             print("Changing to GUIDED mode")
-            self.mode = VehicleMode("GUIDED")
-            while self.mode != 'GUIDED':
+            self.vehicle.mode = VehicleMode("GUIDED")
+            while self.vehicle.mode != 'GUIDED':
                 print(" Waiting for GUIDED mode...")
                 time.sleep(1)
 
             print("Arming the vehicle")
-            self.armed = True
-            while not self.armed:
+            self.vehicle.armed = True
+            while not self.vehicle.armed:
                 print(" Waiting for arming...")
                 time.sleep(1)
 
