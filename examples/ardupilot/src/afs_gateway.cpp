ed91053dcb98023f7923998e5e06846f8bb60d3e
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2025-03-11 09:57:45 -0500

    More logging...

--- examples/ardupilot/src/afs_gateway.cpp
+++ examples/ardupilot/src/afs_gateway.cpp
@@ -132,11 +132,16 @@ void AFS_Gateway::step(const radl_in_t* i, const radl_in_flags_t* i_f, radl_out_
 					// first time
 					cout << "First time diagnostics status message..." << endl;
 					previous_diagnostics_status_time = this->diagnostics_status_mailbox->header.stamp;
+					cout << "previous_diagnostics_status_time set..." << endl;
 					previous_diagnostics_heartbeat_value = std::stoi(this->diagnostics_status_mailbox->status[2].values[0].value);
+					cout << "previous_diagnostics_heartbeat_value set..." << endl;
 				}
+
 				elapsed_diagnostics_status_duration = ((double)this->diagnostics_status_mailbox->header.stamp.sec - (double)previous_diagnostics_status_time.seconds());
 				elapsed_diagnostics_status_duration += (((double)this->diagnostics_status_mailbox->header.stamp.nanosec - (double)previous_diagnostics_status_time.nanoseconds())/1000000000.0);
+				cout << "elapsed_diagnostics_status_duration set..." << endl;
 				current_diagnostics_heartbeat_value = std::stoi(this->diagnostics_status_mailbox->status[2].values[0].value);
+				cout << "current_diagnostics_heartbeat_value set..." << endl;
 				if ((current_diagnostics_heartbeat_value - previous_diagnostics_heartbeat_value) >= ((int)(elapsed_diagnostics_status_duration))){
 					// No Hearbeat Loss
 					current_heartbeat_loss_duration = 0.0;
