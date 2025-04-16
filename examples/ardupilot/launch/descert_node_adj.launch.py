47a971957b317a192d25e1c21afb1fb97575114f
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2025-04-16 09:25:00 -0500

    Revert namespace changes

--- examples/ardupilot/launch/descert_node_adj.launch.py
+++ examples/ardupilot/launch/descert_node_adj.launch.py
@@ -50,9 +50,8 @@ def generate_launch_description():
         package='mavros',
         executable='mavros_node',
         name='mavros',
-        namespace='/uas1/mavros',
         parameters=node_params,
-        arguments=['--ros-args', '--enclave', '/uas1/mavros']
+        arguments=['--ros-args', '--enclave', '/afs/mavros']
     )
 
     ld.add_action(mavros_node)
