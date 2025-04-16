74a1ee934c5d741c65dcab5be0e0ef5adce54438
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2025-04-16 10:13:09 -0500

    Cast fence status msgid value

--- examples/ardupilot/src/afs_gateway.cpp
+++ examples/ardupilot/src/afs_gateway.cpp
@@ -251,7 +251,8 @@ void AFS_Gateway::mavros_battery_state_callback(const sensor_msgs::msg::BatteryS
 
 void AFS_Gateway::mavlink_fence_status_callback(const mavros_msgs::msg::Mavlink::ConstSharedPtr fs)
 {
-	if (fs->msgid == MAVLINK_MSG_ID_FENCE_STATUS)  // FENCE_STATUS
+	fence_status_msgid = static_cast<int>(MAVLINK_MSG_ID_FENCE_STATUS)
+	if (fs->msgid == fence_status_msgid)  // FENCE_STATUS
 	{
 		mavlink_message_t mavlink_msg;
 
